c4712506d187806c6f81c9ed12f8e18847501cfd
Orlo Montesdeoca <76452632+itsOrlo@users.noreply.github.com>
2023-10-16 22:22:09 +0000

    espacios con {" "}

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
               Como entusiasta de la tecnología, me comprometo con el dinámico
               panorama de la era digital. Mi dedicación radica en explorar el
               extenso universo tecnológico y <b>desarrollar</b> soluciones
-              innovadoras mediante la implementación de 
+              innovadoras mediante la implementación de {" "}
               <b>código</b> y <b> diseño</b>. Constantemente persigo desafíos que
               promuevan mi continua formación y progreso en este estimulante
               campo.
@@ -30,7 +30,7 @@ function App() {
           empresa="WebCats"
           testimonio={
             <>
-              Soy Ricardo Zaldumbide, un joven apasionado por la 
+              Soy Ricardo Zaldumbide, un joven apasionado por la {" "}
               <b>programación</b> y la <b>electrónica</b>. Mi orgullo radica en
               mi capacidad para perseguir objetivos con tenacidad y habilidad
               para colaborar eficazmente con otros desarrolladores.
@@ -44,10 +44,10 @@ function App() {
           empresa="WebCats"
           testimonio={
             <>
-              Tengo una pasión por la programación, trato de encontrar la 
+              Tengo una pasión por la programación, trato de encontrar la {" "}
               <b>mejor solución</b> y no me gusta rendirme. Dominio en
               conocimientos de TI capaz de utilizar <b>metodos, tecnicas y
-              herramientas de alto invel</b> para el desarrollo de software, con
+              herramientas de alto nivel</b> para el desarrollo de software, con
               capacidad de analisis y resolucion de problemas.
             </>
           }
