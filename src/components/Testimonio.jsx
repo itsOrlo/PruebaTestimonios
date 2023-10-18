023e4dd23c67fc69bf05cd97f6f08d1900b8fb4a
itsOrlo <orlo.montesdeoca@gmail.com>
2023-10-18 09:26:41 -0500

    parámetros de una función

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,26 +1,29 @@
 import "../stylesheets/Testimonios.css";
 
 /* Exportación nos deja solo un elemento "export" */
-function Testimonio(props) {
+function Testimonio({ imagen, nombre, testimonio, empresa, cargo }) {
+
+  /* props trae a todos los parámetros y se llaman mediante "." "props.imagen" */
+
   return (
     <div className="contenedor-testimonios">
       <img
         className="imagen-testimonio"
 
         /* Obligatorio el uso de "{" y "`" */
-        src={`imgs/testimonio_${props.imagen}.jpg`}
+        src={`imgs/testimonio_${imagen}.jpg`}
 
-        alt={`Foto de ${props.nombre}`}
+        alt={`Foto de ${nombre}`}
         
       />
 
       <div className="contenedor-texto-testimonio">
-        <p className="texto-testimonio"> &quot;{props.testimonio}&quot;</p>
+        <p className="texto-testimonio"> &quot;{testimonio}&quot;</p>
         <p className="nombre-testimonio">
-          <strong>{props.nombre}</strong>
+          <strong>{nombre}</strong>
         </p>
         <p className="cargo-testimonio">
-          {props.cargo} en <strong>{props.empresa}</strong>
+          {cargo} en <strong>{empresa}</strong>
         </p>
       </div>
     </div>
